afaa54d21d959495704e5e5c5ab9ff160059cd53
Angelo Fontana <angelorfontana@outlook.com>
2026-02-03 20:34:48 -0300

    infra

--- APPInfraEstructure/Front/Program.cs
+++ APPInfraEstructure/Front/Program.cs
@@ -1,40 +1,18 @@
 using System.Net;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
 {
-    // HTTP sempre ativo (interno)
-    //    options.Listen(
-    //        IPAddress.Parse(GS.I.MYC.HttpIPListen),
-    //        GS.I.MYC.HttpPortListen
-    //    );
+    // Limites continuam sendo responsabilidade da aplicação
+    options.Limits.MaxConcurrentConnections =
+        GS.I.MYC.MaxConcurrentConnections;
 
-    options.ListenAnyIP(8080);
+    options.Limits.MaxConcurrentUpgradedConnections =
+        GS.I.MYC.MaxConcurrentUpgradedConnections;
 
-
-    //// HTTPS só se certificado existir
-    //if (
-    //    !string.IsNullOrWhiteSpace(GS.I.MYC.HttpsPathCertificado) &&
-    //    File.Exists(GS.I.MYC.HttpsPathCertificado)
-    //)
-    //{
-    //    options.Listen(
-    //        IPAddress.Parse(GS.I.MYC.HttpsIPListen),
-    //        GS.I.MYC.HttpsPortListen,
-    //        listenOptions =>
-    //        {
-    //            listenOptions.UseHttps(
-    //                GS.I.MYC.HttpsPathCertificado,
-    //                GS.I.MYC.HttpssenhaCertificado
-    //            );
-    //        }
-    //    );
-    //}
-
-    options.Limits.MaxConcurrentConnections = GS.I.MYC.MaxConcurrentConnections;
-    options.Limits.MaxConcurrentUpgradedConnections = GS.I.MYC.MaxConcurrentUpgradedConnections;
-    options.Limits.MaxRequestBodySize = GS.I.MYC.MaxRequestBodySize;
+    options.Limits.MaxRequestBodySize =
+        GS.I.MYC.MaxRequestBodySize;
 });
 
 var app = builder.Build();
