f6cb7a1f9394935ab9037c253acf9d89d8c8c768
Angelo Fontana <angelorfontana@outlook.com>
2026-02-04 23:05:28 -0300

    infra

--- Apps/AppClinicas/obj/Debug/net8.0/AppClinicas.AssemblyInfo.cs
+++ Apps/AppClinicas/obj/Debug/net8.0/AppClinicas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppClinicas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9ee7240cc5689a61a516dc9a31a088607922fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppClinicas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppClinicas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
