b3ed7e2fac689977d739df1ebe00904de7a345cc
Angelo Fontana <angelorfontana@outlook.com>
2026-02-14 11:09:41 -0300

    infra

--- APPInfraEstructure/Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
+++ APPInfraEstructure/Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6451865d9c17388ca6127a369b3b376efc989bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- APPInfraEstructure/Migration/Dominio/IA/AiContextBuilder.cs
+++ APPInfraEstructure/Migration/Dominio/IA/AiContextBuilder.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
 using System.Text;
 
