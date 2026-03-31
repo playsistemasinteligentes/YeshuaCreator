9b0763ef976abebd0f4c7a31dc0e38948335e443
AngeloRicardoFontana <angelo.fontana@multisoftware.com.br>
2026-03-31 15:39:01 -0300

    Infra

--- APPInfraEstructure/Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
+++ APPInfraEstructure/Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1481ad6baffcf4ab2cb72171196e91c403928bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9b87b9745bf12a9b2deb2915b450488e38b7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
