1e2c15dc8c21f67dcfa1e204e10dcc08f3da2dec
AngeloRicardoFontana <angelo.fontana@multisoftware.com.br>
2026-05-04 19:17:09 -0300

    infra

--- AppAplication/Command/obj/Debug/net8.0/Command.AssemblyInfo.cs
+++ AppAplication/Command/obj/Debug/net8.0/Command.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Command")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e03785f2180be4a2dd9257a345cf0d3cd7ea1a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cc7a7e19caa9e43d20fede09546b0124d4b018")]
 [assembly: System.Reflection.AssemblyProductAttribute("Command")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Command")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
