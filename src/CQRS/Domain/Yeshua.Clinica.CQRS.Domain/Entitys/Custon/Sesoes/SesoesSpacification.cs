227a871340ba194297e7913a4fac8423c2a1c3c4
AngeloRicardoFontana <angelo.fontana@multisoftware.com.br>
2026-08-06 19:38:59 -0300

    Dominio

---
+++ src/CQRS/Domain/Yeshua.Clinica.CQRS.Domain/Entitys/Custon/Sesoes/SesoesSpacification.cs
@@ -0,0 +1,49 @@
+using Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entitys.Custon
+{
+    public class EmConciliacaoDeHorariosSpec : ISpecification<SesoesEntity>
+    {
+        public string Name => nameof(EmConciliacaoDeHorariosSpec);
+
+        public bool IsSatisfiedBy(SesoesEntity entity)
+        {
+            return entity.StatusAgendamento == 0 || entity.PacienteId != null && entity.DataFim == null;
+        }
+    }
+
+    public class ConfirmadaSpec : ISpecification<SesoesEntity>
+    {
+        public string Name => nameof(ConfirmadaSpec);
+
+        public bool IsSatisfiedBy(SesoesEntity entity)
+        {
+            return entity.StatusAgendamento == 1;
+        }
+    }
+
+    public class RealizadaSpec : ISpecification<SesoesEntity>
+    {
+        public string Name => nameof(RealizadaSpec);
+
+        public bool IsSatisfiedBy(SesoesEntity entity)
+        {
+            return entity.StatusAgendamento == 2;
+        }
+    }
+
+    public class CanceladaSpec : ISpecification<SesoesEntity>
+    {
+        public string Name => nameof(CanceladaSpec);
+
+        public bool IsSatisfiedBy(SesoesEntity entity)
+        {
+            return entity.StatusAgendamento == 3;
+        }
+    }
+}
--- src/CQRS/Domain/Yeshua.Clinica.CQRS.Domain/Entitys/Custon/yFileUpload/IyFileUploadEntity.cs
+++ src/CQRS/Domain/Yeshua.Clinica.CQRS.Domain/Entitys/Custon/yFileUpload/IyFileUploadEntity.cs
@@ -1,8 +1,16 @@
 ﻿
-                namespace Dominio.Entitys
-                {
-            public partial class yFileUploadEntity
+namespace Dominio.Entitys
 {
-}
+    public partial class yFileUploadEntity
+    {
+        public yFileUploadEntity()
+        {
+        }
+
+        public yFileUploadEntity getProxy(int id)
+        {
+            return new yFileUploadEntity() { Id = id };
+        }
+    }
 }
 //Dominio.Schemas.CQRS.SourceCodeEntityMigration
